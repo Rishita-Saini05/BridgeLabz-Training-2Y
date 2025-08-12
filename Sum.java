d15371b1caf2f1b5bc0bf05c264994d8abdd05be
Rishita-Saini05 <rishita.saini_cs24@gla.ac.in>
2025-08-12 11:58:15 +0530

    .\Sum.java

---
+++ Sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Sum
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter no. 1 = ");
+        int a = obj.nextInt();
+        System.out.print("Enter no. 2 = ");
+        int b = obj.nextInt();
+        int c = a+b;
+        System.out.println("Sum = " + c);
+
+
+    }
+}
